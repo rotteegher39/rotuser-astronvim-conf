7e2d157d6be5c5a9908e5bb01d3452ad71ea26b4
rotteegher <noisetide@gmail.com>
2023-03-16 05:58:05 +1100

    tried to add rust-toold until 6am, hate it

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,7 +9,7 @@ return {
       require("notify").setup {
         background_colour = "#a8888f",
       }
-      require("onedark").load {}
+      require("onedark").load()
     end,
   },
   {
@@ -69,7 +69,19 @@ return {
     end,
   },
   { "khaveesh/vim-fish-syntax" },
-  { "simrat39/rust-tools.nvim" }, -- You can also add new plugins here as well:
+  { "simrat39/rust-tools.nvim", config = function() require("rust-tools").setup({
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", require("rust-tools").hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", require("rust-tools").code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        }
+      }
+    )
+    end,
+  }, -- You can also add new plugins here as well:
   { "ron-rs/ron.vim" },
   {
     "Pocco81/auto-save.nvim",
