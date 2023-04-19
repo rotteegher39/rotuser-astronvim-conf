4593dad936c6e3377302c38d74a36f590e62eb45
rotteegher <noisetide@gmail.com>
2023-04-19 19:16:52 +1000

    all fixed. lsp working. NeoTree migrations fixed

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -68,14 +68,38 @@ return {
       auto_clean_after_session_restore = true,
       close_if_last_window = true,
       source_selector = {
-        winbar = true,
-        content_layout = "center",
-        tab_labels = {
-          filesystem = get_icon "FolderClosed" .. " File",
-          buffers = get_icon "DefaultFile" .. " Bufs",
-          git_status = get_icon "Git" .. " Git",
-          diagnostics = get_icon "Diagnostic" .. " Diagnostic",
-        },
+            winbar = true, -- toggle to show selector on winbar
+            statusline = true, -- toggle to show selector on statusline
+            show_scrolled_off_parent_node = true,                    -- boolean
+            sources = {                                               -- table
+                {
+                    source = "filesystem",                            -- string
+                    display_name = "  Files"                        -- string | nil
+                },
+                {
+                    source = "buffers",                               -- string
+                    display_name = " Buffers"                         -- string | nil
+                },
+                {
+                    source = "git_status",                            -- string
+                    display_name = " Git"                          -- string | nil
+                },
+            },
+            content_layout = "center",                                -- string
+            tabs_layout = "equal",                                    -- string
+            truncation_character = "…",                               -- string
+            tabs_min_width = nil,                                     -- int | nil
+            tabs_max_width = nil,                                     -- int | nil
+            padding = 0,                                              -- int | { left: int, right: int }
+            separator = { left = "▏", right= "▕" },                   -- string | { left: string, right: string, override: string | nil }
+            separator_active = nil,                                   -- string | { left: string, right: string, override: string | nil } | nil
+            show_separator_on_edge = false,                           -- boolean
+            highlight_tab = "NeoTreeTabInactive",                     -- string
+            highlight_tab_active = "NeoTreeTabActive",                -- string
+            highlight_background = "NeoTreeTabInactive",              -- string
+            highlight_separator = "NeoTreeTabSeparatorInactive",      -- string
+            highlight_separator_active = "NeoTreeTabSeparatorActive", -- string
+
       },
       default_component_configs = {
         indent = { padding = 0, indent_size = 1 },
@@ -101,10 +125,9 @@ return {
         },
       },
       window = {
-        width = 30,
+        width = 40,
         mappings = {
           ["<space>"] = false, -- disable space until we figure out which-key disabling
-          ["<S-h>"] = "prev_source",
           ["<S-l>"] = "next_source",
           o = "open",
           O = "system_open",
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,5 +1,7 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
+  enabled = true,
+  event = "BufRead",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     -- local null_ls = require "null-ls"
